b95bfb761a9c1564c74c0f2581aea84b11e9b495
ali rahmani <70445184+alirahmnicode@users.noreply.github.com>
2023-10-26 10:46:35 +0330

    change schema and new method"

--- sql/crud.py
+++ sql/crud.py
@@ -3,12 +3,16 @@ from sqlalchemy.orm import Session
 from . import models, schemas
 
 
-# def get_user(db: Session, user_id: int):
-#     return db.query(models.User).filter(models.User.id == user_id).first()
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+
+def get_users(db: Session):
+    return db.query(models.User).all()
 
 
 def create_user(db: Session, user: schemas.UserCreate):
-    db_user = models.User(**user)
+    db_user = models.User(username=user.username, hashed_password=user.hashed_password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
